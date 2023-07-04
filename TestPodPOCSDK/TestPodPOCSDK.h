2aa2ab6a7693ae51834bfd4eb1435b7648c8cb0a
SonalKachare <sonal@clevertap.com>
2023-07-04 15:57:13 +0530

    Initial Commit

---
+++ TestPodPOCSDK/TestPodPOCSDK.h
@@ -0,0 +1,18 @@
+//
+//  TestPodPOCSDK.h
+//  TestPodPOCSDK
+//
+//  Created by Sonal Kachare on 04/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestPodPOCSDK.
+FOUNDATION_EXPORT double TestPodPOCSDKVersionNumber;
+
+//! Project version string for TestPodPOCSDK.
+FOUNDATION_EXPORT const unsigned char TestPodPOCSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPodPOCSDK/PublicHeader.h>
+
+
